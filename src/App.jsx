a16542db54b70d2bb123208a854d68015569550a
CarmeloMelito <melitocarmelo211@gmail.com>
2025-04-14 09:58:34 +0200

    feat:milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+const actressesApi = "https://www.freetestapi.com/api/v1/actresses";
 
 function App() {
-  return <></>;
+  const [actressesList, setActressesList] = useState([]);
+
+  function fetchActresses() {
+    axios
+      .get(actressesApi)
+      .then((res) => (setActressesList(res.data), console.log(res.data)));
+  }
+
+  useEffect(fetchActresses, []);
+
+  return (
+    <>
+      <span>
+        <ul>
+          {actressesList.map((actress) => (
+            <li key={actress.id}> {actress.name}</li>
+          ))}
+        </ul>
+      </span>
+    </>
+  );
 }
 
 export default App;
