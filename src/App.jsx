77caa8cbcd6bd54423731775c75977481e7e8550
CarmeloMelito <melitocarmelo211@gmail.com>
2025-04-14 10:52:11 +0200

    feat:create card actress milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
+
 import axios from "axios";
+//API ATTRICI
 const actressesApi = "https://www.freetestapi.com/api/v1/actresses";
 
 function App() {
   const [actressesList, setActressesList] = useState([]);
 
   function fetchActresses() {
+    //AXIOS
     axios
       .get(actressesApi)
       .then((res) => (setActressesList(res.data), console.log(res.data)));
@@ -15,12 +18,23 @@ function App() {
 
   return (
     <>
+      <h1>ATTRICI</h1>
       <span>
-        <ul>
-          {actressesList.map((actress) => (
-            <li key={actress.id}> {actress.name}</li>
-          ))}
-        </ul>
+        {actressesList.map((actress) => (
+          //CARD
+          <div className="actress-card" key={actress.id}>
+            <img
+              src={actress.image}
+              alt={actress.name}
+              className="actress-image"
+            />
+            <h3 className="actress-name">{actress.name}</h3>
+            <p className="actress-info">{actress.birth_year}</p>
+            <p className="actress-nat">{actress.nationality}</p>
+            <p className="actress-bio">{actress.biography}</p>
+            <p className="actress-awards">{actress.awards}</p>
+          </div>
+        ))}
       </span>
     </>
   );
